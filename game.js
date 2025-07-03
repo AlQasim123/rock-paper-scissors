4d864de32848b75337fb30cfd69c98ad7a6134c8
Islam <eslambnalham@gmail.com>
2025-07-03 16:35:21 +0300

    Create the function playRound & fix typo
    the playRound function has the logic of the game
    the typo in the getHumanChoise function

--- game.js
+++ game.js
@@ -9,5 +9,38 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Rock, Paper, scissors? ")
+    return prompt("Rock, Paper, Scissors?");
 }
+
+let humanScore = computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    // convert the humanChoice to lower case
+    humanChoice = humanChoice.toLowerCase();
+    // convert human an computer choices to a title (start wit a cap letter)
+    let humanWord = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1); 
+    let computerWord = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1); 
+    // a var to hold the result of the round
+    let msg;
+    // game logic draw or computer win else human win
+    if (humanChoice === computerChoice) {
+        msg = `Draw`;
+
+    } else if (humanChoice === "rock" && computerChoice == "paper" ||
+        humanChoice === "paper" && computerChoice == "scissors" ||
+        humanChoice === "scissors" && computerChoice == "rock"
+    ) {
+        msg = `You lose! ${computerWord} beats ${humanWord}`;
+        computerScore++;
+   
+    } else {
+        msg = `You Win! ${humanWord} beats ${computerWord}`;
+        humanScore++;
+    }
+    console.log(msg);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
