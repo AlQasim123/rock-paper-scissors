a91006ed966ea764fe1826dc93fca37db1d9853d
Islam <eslambnalham@gmail.com>
2025-07-03 15:54:46 +0300

    Create the function getComputerChoice
    the function take no parametars, and return
    randomly three values

---
+++ game.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0) {
+        return "rock";
+    } else if (num === 1) {
+        return "paper";
+    }
+    return "scissors";
+}
